4150d05cd953b475d4018ff8eb786a6a811006b7
Marijn Bary <186516@novacollege.nl>
2023-03-14 10:39:32 +0100

    opdracht 20

---
+++ site/fifa.php
@@ -0,0 +1,34 @@
+<?php
+
+$best_soccer_players = [
+    "goalkeeper" => [
+        "Manuel Neuer" => ["Country" => "Germany", "Years Active" => "2006-present"],
+        "Jan Oblak" => ["Country" => "Slovenia", "Years Active" => "2010-present"],
+        "David de Gea" => ["Country" => "Spain", "Years Active" => "2008-present"],
+    ],
+    "defenders" => [
+        "Sergio Ramos" => ["Country" => "Spain", "Years Active" => "2002-present"],
+        "Virgil van Dijk" => ["Country" => "Netherlands", "Years Active" => "2011-present"],
+        "Marcelo" => ["Country" => "Brazil", "Years Active" => "2005-present"],
+    ],
+    "midfielders" => [
+        "Luka Modric" => ["Country" => "Croatia", "Years Active" => "2003-present"],
+        "Kevin De Bruyne" => ["Country" => "Belgium", "Years Active" => "2008-present"],
+        "N'Golo Kante" => ["Country" => "France", "Years Active" => "2011-present"]
+    ],
+    "forwards" => [
+        "Cristiano Ronaldo" => ["Country" => "Portugal", "Years Active" => "2002-present"],
+        "Lionel Messi" => ["Country" => "Argentina", "Years Active" => "2004-present"],
+        "Robert Lewandowski" => ["Country" => "Poland", "Years Active" => "2008-present"]
+    ]
+];
+
+    foreach($best_soccer_players as $key => $groupSpelers) {
+        echo "<h1>" . $key . "</h1>";
+
+        foreach($groupSpelers as $name => $data) {
+            echo $name . "<br>";
+        }
+    }
+
+?>
